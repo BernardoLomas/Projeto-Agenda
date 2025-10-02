814e2fded0314ebe7c19dfcf54d5396b7e69c35a
BernardoLomas <bernardo.lomasb@gmail.com>
2025-10-02 16:40:44 -0300

    Banco de dados integrado

--- server.js
+++ server.js
@@ -6,13 +6,22 @@ app.use(routes);
 const path = require('path');
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extend: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'public')));
+require('dotenv').config();
 
-//===================================================//
+//Conexão com banco de dados
+const mongoose = require('mongoose');
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        console.log('MongoDB conectado!');
+        app.emit('pronto!');
+    })
+    .catch (erro => console.error('Erro ao conectar no MongoDB:', erro));
 
-app.get('/', (req,res) => {
-    res.send(console.log('Olá mundo!'));
+
+app.get('/', (req, res) => {
+    res.send('Olá mundo!');
 })
 
 app.listen(3000, () => {
