4d6fc643a2f091920f8639ce3073485aa817a84b
BernardoLomas <bernardo.lomasb@gmail.com>
2025-10-03 17:12:01 -0300

    Esqueleto do projeto pronto

--- routes.js
+++ routes.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const route = express.Router();
+const homeController = require('./src/controllers/homeController.Js');
+
+// Rotas da home
+route.get('/', homeController.paginaInicial);
+route.post('/', homeController.trataPost);
+
+
 module.exports = route;
